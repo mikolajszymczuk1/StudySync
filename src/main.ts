56b9c1547f1eea79fb675eca71b61979d67f8e7c
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-11-06 20:59:15 +0100

    refactor: fixed slot wrong format

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+import App from '@/App.vue';
+import router from '@/router';
 
 import { IonicVue } from '@ionic/vue';
 
@@ -32,7 +32,7 @@ import '@ionic/vue/css/display.css';
 import '@ionic/vue/css/palettes/dark.system.css';
 
 /* Theme variables */
-import './theme/variables.css';
+import '@/theme/variables.css';
 
 const app = createApp(App).use(IonicVue).use(router);
 
