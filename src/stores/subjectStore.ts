fac3fdba4d3f3a239876b366862ac417bf0fc386
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2024-11-18 11:22:39 +0100

    refactor: added some comments and created README file

--- src/stores/subjectStore.ts
+++ src/stores/subjectStore.ts
@@ -21,6 +21,7 @@ export const useSubjectStore = defineStore('subjectStore', () => {
 
   const evenOdd: Ref<string> = ref('odd');
 
+  /** Get subjects from service and load them to store */
   const loadSubjects = async (): Promise<void> => {
     const userStore = useUserStore();
 
@@ -45,11 +46,25 @@ export const useSubjectStore = defineStore('subjectStore', () => {
     });
   };
 
+  /**
+   * Change single subject day
+   * @param {number} subjectId subject id
+   * @param {string} day day
+   */
   const changeDay = async (subjectId: number, day: string): Promise<void> => {
     const userStore = useUserStore();
     await changeSubjectDay(userStore.user!.id, subjectId, day, userStore.token);
   };
 
+  /**
+   * Add new subject to store
+   * @param {string} name name
+   * @param {string} startTime start time
+   * @param {string} endTime end time
+   * @param {string} evenOdd event odd
+   * @param {string} classNumber class number / class room name
+   * @param {string} day day
+   */
   const add = async (
     name: string,
     startTime: string,
@@ -73,6 +88,16 @@ export const useSubjectStore = defineStore('subjectStore', () => {
     subjects[day as keyof WeekSubjects].push(addedSubject);
   };
 
+  /**
+   * Update subject in store
+   * @param {number} subjectId subject id
+   * @param {string} name name
+   * @param {string} startTime start time
+   * @param {string} endTime end time
+   * @param {string} evenOdd even odd
+   * @param {string} classNumber class number / class room name
+   * @param {string} day day
+   */
   const update = async (
     subjectId: number,
     name: string,
@@ -105,6 +130,11 @@ export const useSubjectStore = defineStore('subjectStore', () => {
     }
   };
 
+  /**
+   * Remove subject from store
+   * @param {number} subjectId subject id
+   * @param {string} day day
+   */
   const remove = async (subjectId: number, day: string): Promise<void> => {
     const userStore = useUserStore();
     await deleteSubject(userStore.user!.id, subjectId, userStore.token);
